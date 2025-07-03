9f90becde0b179272a903f7accd2213a2a911b74
Anant Mogha <rajputanant744@gmail.com>
2025-07-03 19:28:24 +0530

    first

---
+++ chapter 05/01_dict.py
@@ -0,0 +1,16 @@
+marks = {
+    "math": 90,
+    "science": 85,
+    "english": 88,
+    "history": 92,
+}
+
+print("marks['math']")  # Accessing the value for the key 'math'
+print(marks["math"])  # Output: 90                              
+print("marks['science']")  # Accessing the value for the key 'science'
+print(marks["science"])  # Output: 85
+print("marks['english']")  # Accessing the value for the key 'english'
+print(marks["english"])  # Output: 88
+print("marks['history']")  # Accessing the value for the key 'history'
+print(marks["history"])  # Output: 92
+            
---
+++ chapter 05/02_methods.py
@@ -0,0 +1,36 @@
+marks = {
+    "math": 90,
+    "science": 85,
+    "english": 88,
+    "history": 92,
+}
+
+print(marks)
+print(marks.items())
+
+marks = {
+    "math": 90,
+    "science": 85,
+    "english": 88,
+    "history": 92,
+}
+
+print(marks.keys())
+
+marks = {
+    "math": 90,
+    "science": 85,
+    "english": 88,
+    "history": 92,
+}
+
+print(marks.values())
+
+marks = {
+    "math": 90,
+    "science": 85,
+    "english": 88,
+    "history": 92,
+}
+marks.update({"math": 99})
+print(marks)
